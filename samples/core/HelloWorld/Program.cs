cbb652a0e503fb1eee7b67741fe93e01c4ba6800
AVPolyakov <alexandr.polyakov@gmail.com>
2018-07-08 10:41:02 +0300

    Performance long table

--- samples/core/HelloWorld/Program.cs
+++ samples/core/HelloWorld/Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using PdfSharp.Pdf;
 using MigraDoc.DocumentObjectModel;
 using MigraDoc.DocumentObjectModel.Fields;
+using MigraDoc.DocumentObjectModel.Tables;
 using MigraDoc.Rendering;
 
 namespace HelloWorld
@@ -13,8 +15,25 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // Create a MigraDoc document.
-            var document = CreateDocument();
+            M1(100);
+            for (int i = 1; i <= 100; i++)
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                var count = 1000*i;
+                M1(count);
+
+                stopwatch.Stop();
+
+                Console.WriteLine($"{count} {stopwatch.ElapsedMilliseconds}");
+            }
+        }
+
+        private static string M1(int count)
+        {
+// Create a MigraDoc document.
+            var document = CreateDocument(count);
 
             // ----- Unicode encoding and font program embedding in MigraDoc is demonstrated here. -----
 
@@ -33,42 +52,56 @@ namespace HelloWorld
             pdfRenderer.RenderDocument();
 
             // Save the document...
-            const string filename = "HelloWorld.pdf";
+            var filename = $"HelloWorld{Guid.NewGuid()}.pdf";
             pdfRenderer.PdfDocument.Save(filename);
-            // ...and start a viewer.
-            Process.Start(filename);
+            return filename;
         }
+
+        static readonly string text2 = "QWEQWE QWETQWTQ WRTWERT QWETQWET QWEQWR ASDASF".ToUpper();
+        public const double BorderWidth = 0.5D;
 
         /// <summary>
         /// Creates an absolutely minimalistic document.
         /// </summary>
-        static Document CreateDocument()
+        /// <param name="count"></param>
+        static Document CreateDocument(int count)
         {
             // Create a new MigraDoc document.
             var document = new Document();
 
-            document.Styles[StyleNames.Normal].Font.Name = "Lucida Sans";
 
             // Add a section to the document.
             var section = document.AddSection();
 
-            // Add a paragraph to the section.
-            var paragraph = section.AddParagraph();
+            section.PageSetup.TopMargin = Unit.FromCentimeter(1.2);
+            section.PageSetup.BottomMargin = Unit.FromCentimeter(1);
+            section.PageSetup.LeftMargin = Unit.FromCentimeter(2);
+            section.PageSetup.RightMargin = Unit.FromCentimeter(1);
+            section.PageSetup.Orientation = Orientation.Landscape;
 
-            // Set font color.
-            //paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
-            paragraph.Format.Font.Color = Colors.DarkBlue;
+            var table = new Table();
+            table.Borders.Width = BorderWidth;
+            table.LeftPadding = table.RightPadding = 0;
+            table.Format.Font.Name = "Times New Roman";
+            table.Format.Font.Size = 8;
+            var columnCount = 14;
+            for (var i = 0; i < columnCount; i++)
+                table.AddColumn(54.0607424071991);
 
-            // Add some text to the paragraph.
-            paragraph.AddFormattedText("Hello, World!", TextFormat.Bold);
+            for (var j = 0; j < count; j++)
+            {
+                var r = table.AddRow();
+                for (var i = 0; i < columnCount; i++)
+                {
+                    var paragraph = new Paragraph();
+                    paragraph.Format.LeftIndent = Unit.FromCentimeter(0.05);
+                    paragraph.Format.RightIndent = Unit.FromCentimeter(0.05);
+                    paragraph.AddText(text2);
+                    r[i].Add(paragraph);
+                }
+            }
 
-            // Create the primary footer.
-            var footer = section.Footers.Primary;
-
-            // Add content to footer.
-            paragraph = footer.AddParagraph();
-            paragraph.Add(new DateField() { Format = "yyyy/MM/dd HH:mm:ss" });
-            paragraph.Format.Alignment = ParagraphAlignment.Center;
+            document.LastSection.Add(table);
 
             return document;
         }
